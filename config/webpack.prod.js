fbaa0c52d449d52db8713b1839a8ad100d2c7ff5
Vincent Euloge <vincent.euloge@gmail.com>
2016-10-31 08:06:12 +0100

    add electron

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -11,7 +11,7 @@ module.exports = webpackMerge(commonConfig, {
 
   output: {
     path: helpers.root('dist'),
-    publicPath: '/',
+    publicPath: helpers.root('dist'),
     filename: '[name].[hash].js',
     chunkFilename: '[id].[hash].chunk.js'
   },
